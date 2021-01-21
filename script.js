bbfa91532d138c01d3689e3c056d7ff891aa197a
Pranjal Chaplot <pranjalchaplot@gmail.com>
2021-01-21 20:34:20 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// function background(){
+//     document.body.style.backgroundImage = "url(img/background_img.jpg)"
+//     document.body.style.backgroundSize = "1920px 1080px"
+// }
+// background();
